2c434aeba814fc6ac0e26b2558c2ef79c138c392
greg <gregorycusack@gmail.com>
2020-05-19 09:15:07 -0700

    add locks gcpu global

--- stats/global/cpu_g.cpp
+++ stats/global/cpu_g.cpp
@@ -24,11 +24,38 @@ void ec::global::stats::cpu::decr_unallocated_rt(uint64_t _decr) {
     std::unique_lock<std::mutex> lk(unalloc_lock);
     std::cout << "Decr unalloc: " << unallocated_rt << std::endl;
     if( ((int64_t) unallocated_rt - (int64_t) _decr) < 0) {
+        std::cout << "unalloc - decr < 0. decr: " << _decr << std::endl;
         unallocated_rt = 0;
-	std::cout << "unalloc - decr < 0. decr: " << _decr << std::endl;
     }
     else {
         unallocated_rt -= _decr;
     }
     std::cout << "decr unalloc post update: " << unallocated_rt << std::endl;
 }
+
+
+void ec::global::stats::cpu::incr_unalloacted_rt(uint64_t _incr) {
+    std::unique_lock<std::mutex> lk(unalloc_lock);
+    unallocated_rt += _incr;
+}
+
+void ec::global::stats::cpu::set_unallocated_rt(uint64_t _val) {
+    std::unique_lock<std::mutex> lk(unalloc_lock);
+    unallocated_rt = _val;
+}
+
+uint64_t ec::global::stats::cpu::get_unallocated_rt() {
+    std::unique_lock<std::mutex> lk(unalloc_lock);
+    return unallocated_rt;
+}
+
+//ec::global::stats::cpu::cpu(const ec::global::stats::cpu &_cpu) {
+//    quota = _cpu.quota;
+//    period = _cpu.period;
+//    slice_size = _cpu.slice_size;
+//    runtime_remaining = _cpu.runtime_remaining;
+//    unallocated_rt = _cpu.unallocated_rt;
+//    total_cpu = _cpu.total_cpu;
+//    overrun = _cpu.overrun;
+//    unalloc_lock = _cpu.unalloc_lock;
+//}
--- stats/global/cpu_g.h
+++ stats/global/cpu_g.h
@@ -16,6 +16,7 @@ namespace ec {
             public:
                 cpu();
                 cpu(uint64_t _period, int64_t _quota, uint64_t _slice_size);
+                cpu(const cpu &_cpu);
 
                 /**
                  * GETTERS
@@ -24,7 +25,7 @@ namespace ec {
                 uint64_t get_period() { return period; }
                 uint64_t get_slice() { return slice_size; }
                 uint64_t get_runtime_remaining() { return runtime_remaining; }
-                uint64_t get_unallocated_rt() { return unallocated_rt; }
+                uint64_t get_unallocated_rt();// { return unallocated_rt; }
                 uint64_t get_total_cpu() { return total_cpu; }
                 uint64_t get_overrun() { return overrun; }
 
@@ -37,15 +38,17 @@ namespace ec {
                  void set_runtime_remaining(uint64_t _rt_remain) { runtime_remaining = _rt_remain; }
                  void decr_runtime_remaining(uint64_t _to_decr) { runtime_remaining -= _to_decr; }
                  uint64_t refill_runtime();
-                 void incr_unalloacted_rt(uint64_t _incr) { unallocated_rt += _incr; }
+                 void incr_unalloacted_rt(uint64_t _incr);// { unallocated_rt += _incr; }
                  void decr_unallocated_rt(uint64_t _decr);// { unallocated_rt -= _decr; }
-                 void set_unallocated_rt(uint64_t _val) { unallocated_rt = _val; }
+                 void set_unallocated_rt(uint64_t _val);// { unallocated_rt = _val; }
                  void set_total_cpu (uint64_t _tot_cpu) { total_cpu = _tot_cpu; }
                  void incr_total_cpu (uint64_t _incr) { total_cpu += _incr; }
                  void decr_total_cpu (uint64_t _decr) { total_cpu -= _decr; }
                  void incr_overrun(uint64_t _incr) { overrun += _incr; }
                  void decr_overrun(uint64_t _decr) { overrun -= _decr; }
                  void set_overrun(uint64_t _val) { overrun = _val; }
+
+
 
 
             private:
@@ -57,7 +60,7 @@ namespace ec {
                 uint64_t total_cpu;
                 uint64_t overrun;
 
-		std::mutex unalloc_lock;
+		        std::mutex unalloc_lock;
 //                uint32_t cpu_limit_in_cores;
 //                uint32_t cpu_current_usage_in_cores;
 //                uint32_t nr_need_cpu;
