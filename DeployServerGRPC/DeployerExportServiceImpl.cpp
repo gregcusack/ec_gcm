f7bed3b41ca3b8f2c5782f4026b0e5cb47ce159b
gregcusack <gcusack@netflix.com>
2021-05-07 20:34:17 -0600

    debugging sciddockeridmatcherthread

--- DeployServerGRPC/DeployerExportServiceImpl.cpp
+++ DeployServerGRPC/DeployerExportServiceImpl.cpp
@@ -204,12 +204,16 @@ void ec::rpc::DeployerExportServiceImpl::scIdToDockerIdMatcherThread(void* argum
     auto threadArgs = reinterpret_cast<matchingThreadArgs*>(arguments);
     std::unique_lock<std::mutex> lk(cv_mtx);
     cv.wait(lk, [this, threadArgs] {
-        auto itr = ec->get_sc_ac_map_for_update()->find(threadArgs->sc_id);
         SPDLOG_DEBUG("wait for sc_id to exist in sc_ac_map: {}, d_id: {}", threadArgs->sc_id, threadArgs->docker_id);
-        if(itr == ec->get_sc_ac_map_for_update()->end()) {
-            SPDLOG_DEBUG("bruvvvv");
-        }
-        return itr != ec->get_sc_ac_map_for_update()->end();
+        SPDLOG_DEBUG("sc_ac_map size: {}", ec->get_sc_ac_map().size());
+        SPDLOG_DEBUG("sc_ac_map_for_update_size: {}", ec->get_sc_ac_map_for_update()->size());
+        return ec->get_sc_ac_map_for_update()->end() != ec->get_sc_ac_map_for_update()->find(threadArgs->sc_id);
+//        auto itr = ec->get_sc_ac_map_for_update()->find(threadArgs->sc_id);
+//        SPDLOG_DEBUG("wait for sc_id to exist in sc_ac_map: {}, d_id: {}", threadArgs->sc_id, threadArgs->docker_id);
+//        if(itr == ec->get_sc_ac_map_for_update()->end()) {
+//            SPDLOG_DEBUG("bruvvvv");
+//        }
+//        return itr != ec->get_sc_ac_map_for_update()->end();
     });
 
     SPDLOG_DEBUG("out of match part!");
