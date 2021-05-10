acc3080bc381df59ee2a9b7cc3b1a4241ec7fde6
greg <gregorycusack@gmail.com>
2021-05-09 20:19:23 -0600

    add back in send lock

--- Agents/AgentClient.cpp
+++ Agents/AgentClient.cpp
@@ -7,9 +7,9 @@
 ec::AgentClient::AgentClient(const ec::Agent *_agent, int _sockfd)
     : agent(_agent), sockfd_new(_sockfd) {}
 
-int64_t ec::AgentClient::send_request(const struct msg_struct::ECMessage &msg) const {
+int64_t ec::AgentClient::send_request(const struct msg_struct::ECMessage &msg) { //const {
     int64_t ret;
-//    std::unique_lock<std::mutex> lk(sendlock);
+    std::unique_lock<std::mutex> lk(sendlock);
     ec::Facade::ProtoBufFacade::ProtoBuf::sendMessage(sockfd_new, msg);
     msg_struct::ECMessage rx_msg;
     ec::Facade::ProtoBufFacade::ProtoBuf::recvMessage(sockfd_new, rx_msg);
