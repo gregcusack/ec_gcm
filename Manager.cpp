0a492af8255aa89124e94451094adcad3ba53afb
greg <gregorycusack@gmail.com>
2021-11-08 15:04:03 -0700

    turned determine_mem_limit_for_new_pod() into detached thread. seems to work

--- Manager.cpp
+++ Manager.cpp
@@ -478,10 +478,10 @@ int ec::Manager::handle_add_cgroup_to_ec(const ec::msg_t *req, ec::msg_t *res, u
     }
 
     //update pod mem limit
-//    std::thread update_mem_limit_thread(&ec::Manager::determine_mem_limit_for_new_pod, this, sc, fd);
-//    update_mem_limit_thread.detach();
+    std::thread update_mem_limit_thread(&ec::Manager::determine_mem_limit_for_new_pod, this, sc, fd);
+    update_mem_limit_thread.detach();
 
-    determine_mem_limit_for_new_pod(sc, fd);
+//    determine_mem_limit_for_new_pod(sc, fd);
 
     SPDLOG_INFO("total pods added to map: {}", ec_get_num_subcontainers());
     res->request += 1; //giveback (or send back)
